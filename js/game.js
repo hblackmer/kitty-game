805ee356106c70ef735b80bfed827d680a178581
Helene <hcblackmer@gmail.com>
2021-08-28 16:40:28 -0700

    Beginnings of the game! Added kitty and pepper animations as well as jumping and scoreboard functionality.

---
+++ js/game.js
@@ -0,0 +1,24 @@
+var kitty = document.querySelector("#kitty");
+var pepper = document.querySelector("#pepper");
+var counter = 0;
+
+function jump(){
+    if(kitty.classList == "animate"){return}
+    kitty.classList.add("animate");
+    setTimeout(function(){
+        kitty.classList.remove("animate");
+    },300);
+}
+
+var game = setInterval(function() {
+    let kittyTop = parseInt(window.getComputedStyle(kitty).getPropertyValue("top"));
+    let pepperLeft = parseInt(window.getComputedStyle(pepper).getPropertyValue("left"));
+    if(pepperLeft<20 && pepperLeft>-20 && kittyTop>=130){
+        pepper.style.animation = "none";
+        counter=0;
+        pepper.style.animation = "pepper 1s infinite linear";
+    }else{
+        counter++;
+        document.getElementById("score").innerHTML = Math.floor(counter/100);
+    }
+}, 10);
