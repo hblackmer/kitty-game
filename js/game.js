40b8e3e729f23783a7f3d21daa0a15ff9456ab61
Helene <hcblackmer@gmail.com>
2021-09-11 15:10:43 -0700

    Prevent cat from jumping until start is initiated. Clean up to JavaScript file.

--- js/game.js
+++ js/game.js
@@ -1,68 +1,58 @@
-var kitty = document.querySelector("#kitty");
-var pepper = document.querySelector("#pepper");
-var counter = 0;
-let highScore = 0;
 let start = document.getElementById("start");
+let kitty = document.querySelector("#kitty");
+let pepper = document.querySelector("#pepper");
+let counter = 0;
+let highScore = 0;
 
+// Game paused until START button is clicked or ENTER keyboard input.
 pepper.style.webkitAnimationPlayState = "paused";
+kitty.style.webkitAnimationPlayState = "paused";
 
-function jump(){
-    if(kitty.classList == "animate"){return}
-    kitty.classList.add("animate");
-    setTimeout(function(){
-        kitty.classList.remove("animate");
-    },300);
+// Kitty jump animation.
+function jump() {
+    if (kitty.style.webkitAnimationPlayState === "running") {
+        if (kitty.classList == "animate") { return }
+        kitty.classList.add("animate");
+        setTimeout(function () {
+            kitty.classList.remove("animate");
+        }, 300);
+    }
 }
 
+// Kitty jumps with UP or SPACEBAR keyboard input. Option to start game with ENTER keyboard input.
 document.addEventListener("keydown", function (e) {
     if (e.keyCode === 32 || e.keyCode === 38) {
         jump();
     }
+    if (e.keyCode == 13) {
+        gameLogic();
+    }
 });
 
-start.onclick = function(){
-    pepper.style.webkitAnimationPlayState = "running";
-    start.style.display = "none";
-    var game = setInterval(function() {
-        let kittyTop = parseInt(window.getComputedStyle(kitty).getPropertyValue("top"));
-        let pepperLeft = parseInt(window.getComputedStyle(pepper).getPropertyValue("left"));
-        if(pepperLeft<20 && pepperLeft>-20 && kittyTop>=130){
-            pepper.style.animation = "none";
-            counter=0;
-            pepper.style.animation = "pepper 1s infinite linear";
-        }else{
-            counter++;
-            let score = Math.floor(counter/100);
-            document.getElementById("score").innerHTML = score
-            if(highScore < score) {
-                highScore = score;
-                document.getElementById("highScore").innerHTML = highScore;
-            }
-        }
-    }, 10);
-}
+// Start game with START button click.
+start.addEventListener("click", function () {
+    gameLogic();
+});
 
-document.body.onkeyup = function(e){
-    if(e.keyCode == 13){
+const gameLogic = function () {
+    const game = setInterval(function () {
         pepper.style.webkitAnimationPlayState = "running";
-    start.style.display = "none";
-    var game = setInterval(function() {
+        start.style.display = "none";
         let kittyTop = parseInt(window.getComputedStyle(kitty).getPropertyValue("top"));
         let pepperLeft = parseInt(window.getComputedStyle(pepper).getPropertyValue("left"));
-        if(pepperLeft<20 && pepperLeft>-20 && kittyTop>=130){
+        kitty.style.webkitAnimationPlayState = "running";
+        if (pepperLeft < 20 && pepperLeft > -20 && kittyTop >= 130) {
             pepper.style.animation = "none";
-            counter=0;
+            counter = 0;
             pepper.style.animation = "pepper 1s infinite linear";
-        }else{
+        } else {
             counter++;
-            let score = Math.floor(counter/100);
+            let score = Math.floor(counter / 100);
             document.getElementById("score").innerHTML = score
-            if(highScore < score) {
+            if (highScore < score) {
                 highScore = score;
                 document.getElementById("highScore").innerHTML = highScore;
             }
         }
     }, 10);
-    }
 }
-
